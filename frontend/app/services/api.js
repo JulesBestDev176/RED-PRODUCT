c8c0d36aaed206d9c0b41bed175752dbaaf9f16b
Souleymane Fall <161875016+JulesBestDev176@users.noreply.github.com>
2024-10-24 13:30:40 +0000

    Update api.js

--- frontend/app/services/api.js
+++ frontend/app/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API_URL = "http://localhost:5000/api";
 //const API_URL = "https://red-product-frontend-peach.vercel.app/api";
-const API_URL = "http://localhost:10000/api";
+const API_URL = "http://localhost:5000/api";
 
 const apiClient = axios.create({
   baseURL: API_URL,
