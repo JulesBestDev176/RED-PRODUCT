65b5a55b3542583a42bf01cf661ff1e721392899
JulesBestDev176 <souleymanefall176@gmail.com>
2024-10-24 09:47:01 +0000

    cors update 5

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api", userRoutes);
 app.use("/api", deviseRoutes);
 app.use("/api", hotelRoutes);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(5000 || process.env.PORT, () => {
   connectDB();
   console.log(
     `Server started at ${
