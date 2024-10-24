fb151630221e6feb021d750ded9033d60254e3d6
Souleymane Fall <161875016+JulesBestDev176@users.noreply.github.com>
2024-10-24 12:54:01 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,5 +38,7 @@ app.use("/api", hotelRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   connectDB();
-  console.log(`${process.env.PORT} ? "Server started at ${process.env.PORT}" : "http://localhost:5000"`);
+  const port = process.env.PORT || 5000; // Détermine le port à utiliser
+  console.log(`Server started at http://localhost:${port}`); // Affiche l'URL correcte
 });
+
